57d61b2e5753398f91e2d95a97de4e3797a2d5ef
Md. Al amin <alamincsecu@gmail.com>
2022-04-04 05:05:24 +0600

    card and its style

--- lib/main.dart
+++ lib/main.dart
@@ -4,128 +4,43 @@ void main(List<String> args) {
   runApp(MyApp());
 }
 
+String titleText = 'Expense Tracker';
+
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  static const String _title = 'CardWidget';
+  MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _title,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(_title),
-        ),
-        body: MyCard(),
-      ),
+      title: titleText,
+      home: const MyHomePage(),
     );
   }
 }
 
-//Creating MyCard Class
-class MyCard extends StatelessWidget {
-  const MyCard({Key? key}) : super(key: key);
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(titleText),
+      ),
+      body: Column(
         children: [
           Card(
-            elevation: 5.0,
-            child: InkWell(
-              splashColor: Colors.blue.withAlpha(30),
-              onTap: () {
-                debugPrint('Card tapped');
-              },
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const ListTile(
-                    leading: Icon(
-                      Icons.album,
-                      color: Colors.blue,
-                    ),
-                    title: Text('Hashimukh'),
-                    subtitle: Text('Band: Shironamhin'),
-                    //isThreeLine: true,
-                    trailing: Icon(
-                      Icons.play_arrow_outlined,
-                      color: Colors.blue,
-                      size: 36.0,
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      OutlinedButton(
-                        onPressed: () {},
-                        child: const Text(
-                          'Download',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 8.0,
-                      ),
-                      OutlinedButton(
-                        onPressed: () {},
-                        child: const Text(
-                          'Play Online',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+            child: Container(
+              width: double.infinity,
+              color: Colors.amberAccent,
+              child: const Text('chart'),
             ),
           ),
           const SizedBox(
-            height: 12.0,
-          ),
-          Card(
-            elevation: 5.0,
-            borderOnForeground: true,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const ListTile(
-                  leading: Icon(
-                    Icons.album,
-                    color: Colors.blue,
-                  ),
-                  title: Text('Bohemian'),
-                  subtitle: Text('Band: Shironamhin'),
-                  //isThreeLine: true,
-                  trailing: Icon(
-                    Icons.play_arrow_outlined,
-                    color: Colors.blue,
-                    size: 36.0,
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    OutlinedButton(
-                      onPressed: () {},
-                      child: const Text(
-                        'Download',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 8.0,
-                    ),
-                    OutlinedButton(
-                      onPressed: () {},
-                      child: const Text(
-                        'Play Online',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+            width: double.minPositive,
+            child: Card(
+              color: Colors.blue,
+              child: Text('List of transaction'),
             ),
           ),
         ],
