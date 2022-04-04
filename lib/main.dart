f544c7bcb00d3d76ade66120d19fe843ee5ca57f
Md. Al amin <alamincsecu@gmail.com>
2022-04-04 09:27:57 +0600

    Adding transaction and mapping data into widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
@@ -13,13 +14,28 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: titleText,
-      home: const MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  MyHomePage({Key? key}) : super(key: key);
+
+  List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Egg',
+      amount: 72,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Dart',
+      amount: 200.50,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -28,6 +44,7 @@ class MyHomePage extends StatelessWidget {
         title: Text(titleText),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           Card(
             child: Container(
@@ -36,13 +53,12 @@ class MyHomePage extends StatelessWidget {
               child: const Text('chart'),
             ),
           ),
-          const SizedBox(
-            width: double.minPositive,
-            child: Card(
-              color: Colors.blue,
-              child: Text('List of transaction'),
-            ),
-          ),
+          Column(
+              children: transactions
+                  .map((tx) => Card(
+                        child: Text(tx.id),
+                      ))
+                  .toList()),
         ],
       ),
     );
