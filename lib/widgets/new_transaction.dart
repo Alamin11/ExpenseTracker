ac59ae56860d4a00338159a194e1a34b3daee1c0
Md. Al amin <alamincsecu@gmail.com>
2022-04-10 09:31:47 +0600

    Styling and validating the transactions

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -2,10 +2,21 @@ import 'package:flutter/material.dart';
 
 class NewTransaction extends StatelessWidget {
   //NewTransaction({Key? key, required this.newTransaction}) : super(key: key);
-  final Function newTransaction;
-  NewTransaction(this.newTransaction, {Key? key}) : super(key: key);
+  final Function addTx;
+  NewTransaction(this.addTx, {Key? key}) : super(key: key);
   final titleController = TextEditingController();
   final amountController = TextEditingController();
+  void submitTx() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+    addTx(
+      enteredTitle,
+      enteredAmount,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +31,8 @@ class NewTransaction extends StatelessWidget {
                 //label: Text('Title'),
                 labelText: 'Title',
               ),
+              keyboardType: TextInputType.text,
+              onSubmitted: (_) => submitTx(), //submitting from soft keyboard
               //onChanged: (value) => titleInput = value,
               controller: titleController,
             ),
@@ -27,21 +40,15 @@ class NewTransaction extends StatelessWidget {
               decoration: const InputDecoration(
                 labelText: 'Amount',
               ),
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitTx(),
               // onChanged: (value) {
               //   amountInput = value;
               // },
               controller: amountController,
             ),
             TextButton(
-              onPressed: () {
-                // print('titleInput = $titleInput');
-                // print('amountInput = $amountInput');
-
-                // print(titleController.text);
-                // print(amountController.text);
-                newTransaction(
-                    titleController.text, double.parse(amountController.text));
-              },
+              onPressed: submitTx,
               child: const Text('Add transaction'),
             ),
           ],
