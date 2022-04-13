d133744f14b39bff776983dba5dfe14b3caf14bc
Md. Al amin <alamincsecu@gmail.com>
2022-04-13 22:11:54 +0600

    Added custom them, fonts and images with logic

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,13 @@ import '../models/transaction.dart';
 import './widgets/transaction_list.dart';
 
 void main(List<String> args) {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 String titleText = 'Expense Tracker';
 
 class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -19,15 +19,22 @@ class MyApp extends StatelessWidget {
       title: titleText,
       theme: ThemeData(
         primarySwatch: Colors.green,
-        accentColor: Colors.amber,
+        //colorScheme: ColorScheme.fromSwatch(),
+        fontFamily: 'LobsterTwo',
+        appBarTheme: const AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontFamily: 'RobotoCondensed',
+            fontSize: 20.0,
+          ),
+        ),
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -38,7 +45,7 @@ class _MyHomePageState extends State<MyHomePage> {
   // final amountController = TextEditingController();
 
   final List<Transaction> _userTransactions = [
-    Transaction(
+    /*Transaction(
       id: 't1',
       title: 'Egg',
       amount: 72,
@@ -49,7 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
       title: 'Dart',
       amount: 200.50,
       date: DateTime.now(),
-    ),
+    ),*/
   ];
 
   void _AddNewTransaction(String title, double amount) {
@@ -81,7 +88,14 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(titleText),
+        title: Text(
+          titleText,
+          style: const TextStyle(
+            fontFamily: 'RobotoCondensed',
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         actions: [
           IconButton(
             onPressed: () => _startAddNewTransaction(context),
@@ -107,7 +121,7 @@ class _MyHomePageState extends State<MyHomePage> {
       //floatingActionButton: Icon(Icons.add),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
         onPressed: () => _startAddNewTransaction(context),
       ),
     );
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,69 +10,95 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
         height: 340,
-        child: ListView.builder(
-          itemBuilder: (context, index) {
-            return Card(
-              child: Row(
+        child: transactions.isEmpty
+            ? Column(
                 children: [
-                  Container(
-                    width: 100.0,
-                    margin: const EdgeInsets.symmetric(
-                      horizontal: 10,
-                      vertical: 15.0,
-                    ),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Theme.of(context).primaryColor,
-                        width: 2,
-                      ),
-                    ),
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(
-                      //tx.amount.toString(),
-                      '\$ ${transactions[index].amount.toStringAsFixed(2)}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                        color: Theme.of(context).primaryColor,
-                      ),
+                  const SizedBox(
+                    height: 16.0,
+                  ),
+                  const Text(
+                    'No transaction added yet',
+                    style: TextStyle(
+                      //fontFamily: 'RobotoCondensed',
+                      fontSize: 20.0,
                     ),
                   ),
                   const SizedBox(
-                    width: 12.0,
+                    height: 16.0,
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        transactions[index].title,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                          color: Colors.black,
-                        ),
-                      ),
-                      Text(
-                        // DateFormat('dd/MM/yy').format(tx.date),
-                        //DateFormat.yMMMMEEEEd().format(tx.date),
-                        DateFormat.yMEd().format(transactions[index].date),
-                        style: const TextStyle(
-                          fontSize: 15.0,
-                          fontWeight: FontWeight.w300,
-                        ),
-                      ),
-                    ],
+                  SizedBox(
+                    height: 200,
+                    child: Image.asset(
+                      'assets/images/transparentWaiting.gif',
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ],
-              ),
-            );
-          },
-          itemCount: transactions.length,
-          // children: transactions
-          //     .map(
-          //       (tx) =>
-          //     )
-          //     .toList()),
-        ));
+              )
+            : ListView.builder(
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 100.0,
+                          margin: const EdgeInsets.symmetric(
+                            horizontal: 10,
+                            vertical: 15.0,
+                          ),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Theme.of(context).primaryColor,
+                              width: 2,
+                            ),
+                          ),
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                            //tx.amount.toString(),
+                            '\$ ${transactions[index].amount.toStringAsFixed(2)}',
+                            style: TextStyle(
+                              //fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 12.0,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              transactions[index].title,
+                              style: const TextStyle(
+                                //fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                                color: Colors.black,
+                              ),
+                            ),
+                            Text(
+                              // DateFormat('dd/MM/yy').format(tx.date),
+                              //DateFormat.yMMMMEEEEd().format(tx.date),
+                              DateFormat.yMEd()
+                                  .format(transactions[index].date),
+                              style: const TextStyle(
+                                fontSize: 14.0,
+                                fontWeight: FontWeight.w300,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                itemCount: transactions.length,
+                // children: transactions
+                //     .map(
+                //       (tx) =>
+                //     )
+                //     .toList()),
+              ));
   }
 }
